e029e9f9dde0c22cac162c463b2701cb2d3efd20
Andres <aeu1002@alu.ubu.es>
2024-10-09 20:24:13 +0200

    modules

---
+++ modules/archivo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 9/10/2024
+
+@author: Paula Martínez Camarero y Andrés Estévez Ubierna
+"""
+import os
+import pydicom
+import numpy as np
+def Load_Slices()
